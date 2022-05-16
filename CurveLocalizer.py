74cfda9f66330a20202c6366b24529c72a599dce
Shrey <skansal@ucsd.edu>
2022-05-16 15:12:48 -0700

    Added functions for getting window and predicting motion model

--- CurveLocalizer.py
+++ CurveLocalizer.py
@@ -1,3 +1,4 @@
+from re import S
 import numpy as np
 import os,pickle
 from scipy.ndimage import convolve1d
@@ -9,7 +10,7 @@ class CurveLocalizer:
         #         self.map = pickle.load(handle)
         # else:
         self.map = mapDir
-            
+        
         # create a 2d matrix using the map vector:
         # i.e. map = [0,1,2,3,4]
         # 2d map matrix= [[0,1,2],
@@ -22,23 +23,65 @@ class CurveLocalizer:
         
         self.mapMatrix = self.createMapMatrix()
         
+
     def createMapMatrix(self):
-        mapMat = np.zeros((0,self.window_size))
-        for i in range(len(self.map)):
-            idx = list(range(i,i+self.window_size))
-            tempRow = self.map.take(idx, mode='wrap')
-            tempRow = np.reshape(tempRow, (1,self.window_size))
-            mapMat = np.append(mapMat, tempRow, axis=0)
-            print(mapMat)
-        return mapMat
     
+    #     mapMat = np.zeros((0,self.window_size))
+    #     for i in range(len(self.map)):
+    #         idx = list(range(i,i+self.window_size))
+    #         tempRow = self.map.take(idx, mode='wrap')
+    #         tempRow = np.reshape(tempRow, (1,self.window_size))
+    #         mapMat = np.append(mapMat, tempRow, axis=0)
+    #         # print(mapMat)
+    #     return mapMat
+        pass
+    
+
     def findPosition_convolve(self, data):
-        convResult = convolve1d(map, data, mode='wrap')
+
+    #     convResult = convolve1d(map, data, mode='wrap')
         
-        minPosition = np.argmin(convResult)
+    #     minPosition = np.argmin(convResult)
         
-        return minPosition
+    #     return minPosition
+
+        pass
+
+    def predict(self, imu_twist, vesc_twist, dt, X_prev):
+
+        ## gather data form imu and vesc
+        v_theta = imu_twist[-1]
+        v_x = vesc_twist[0]
+        v_y = vesc_twist[1]
+
+        ## euler discretization of differential drive model
+        X = X_prev + dt*np.array([[v_x], [v_y], [v_theta]])
+
+        return X
+
+
+    def convolve(self, map_array, window, start, win_size):
+
+        win_sum = np.sum(window)
+        b = np.convolve(map_array[int(start - 0.5*win_size)::], np.ones(win_size,dtype=int),'valid')
+        curve_num = np.argmin(abs(b-win_sum))
+
+        return curve_num
         
         
     
-            
+    def windowSize(self, vesc_twist, v_max, v_min, max_size, min_size):
+
+        # v_x = vesc_twist[0]
+        # v_y = vesc_twist[1]
+
+        v = max(vesc_twist[0], vesc_twist[1])
+
+        if v > v_max:
+            win_size = max_size
+        elif v < v_min:
+            win_size = min_size
+        else:
+            win_size = v/v_max * max_size
+
+        return int(win_size)
